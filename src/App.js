543497b75297aff9d8da51802a2f051bf1723a27
Jordan <jkwan104@gmail.com>
2022-10-28 17:44:14 -0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './css/App.css';
 import React from 'react';
 import axios from 'axios';
+import Alert from 'react-bootstrap/Alert';
+import Figure from 'react-bootstrap/Figure';
+import Weather from './Weather';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,8 +15,10 @@ class App extends React.Component {
         errorMessage: '',
         lat: '',
         lon: '',
-        mapData: [],
+        mapURL: '',
         center: '',
+        weatherData: [],
+        // cityToDisplay: [],
         }
       }
     // handleInput prevents form from automatically submitting or prevents the page from refreshing 
@@ -30,24 +35,58 @@ class App extends React.Component {
   
       try {
         
-        let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&lat=${this.state.lat}&${this.state.lon}format=json`
-        
-        console.log(url);
+        let LocationURL = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`
+        console.log(LocationURL);
 
-        let cityData = await axios.get(url);
-  
-        console.log(cityData.data[0]);
+        let locationData = await axios.get(LocationURL);
+        
+        let cityToDisplay = locationData.data[0];
+        
+        console.log(cityToDisplay);
+        console.log(cityToDisplay.lat);
+        console.log(cityToDisplay.lon)
+
+
+        let mapURL = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${cityToDisplay.lat},${cityToDisplay.lon}&zoom=14`
+        // https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${cityToDisplay.lat},${cityToDisplay.lon}&zoom=14&markers=icon:small-red-cutout|${cityToDisplay.lat},${cityToDisplay.lon}
+        console.log(mapURL);
+
         this.setState({
-          cityData: cityData.data[0],
+          lat: cityToDisplay.lat,
+          lon: cityToDisplay.lon,
+          cityData: cityToDisplay,
+          map: mapURL,
           error: false,
         });
-      
+        console.log(cityToDisplay);
+      this.getWeatherData(cityToDisplay);
+
       } catch(error){
         console.log(error);
         this.setState({
           error: true,
           errorMessage: error.message
         })
+      }
+    }
+
+    getWeatherData = async (location) => {
+      try {
+        
+        let url = `${process.env.REACT_APP_SERVER}/weather?cityName=${this.state.city}&lat=${location.lat}&lon=${location.lon}`
+  
+        console.log('weather url', url);
+  
+        let weatherData = await axios.get(url)
+  
+        this.setState({
+          weatherData: weatherData.data,
+        });
+      } catch (error) {
+        this.setState({
+          error: true,
+          errorMessage: error.message
+        });
       }
     }
   render() {
@@ -66,11 +105,24 @@ class App extends React.Component {
       {
       this.state.error
       ?
-      <p>{this.state.errorMessage}</p>
-      :
-      <>
-      <p>{this.state.cityData.display_name} {this.state.cityData.lat} {this.state.cityData.lon} {this.state.mapData.center}</p>
-      <img src = {`https://maps.locationiq.com/v3/staticmap/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=14`}  alt='map'/>
+      <Alert variant='warning'>{this.state.errorMessage}</Alert>
+            :
+            this.state.cityData &&
+            <>
+            <Figure>
+              <Figure.Image
+                width={500}
+                height={500}
+                alt="Map of city chosen by the user"
+                src={this.state.map}
+              />
+              <Figure.Caption>
+                {this.state.cityData.display_name} {this.state.cityData.lat}  {this.state.cityData.lon}
+              </Figure.Caption>
+            </Figure>
+            <Weather
+              weatherData={this.state.weatherData}
+            />
       </>
       } 
   </>
